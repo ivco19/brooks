c54f51c1b28d8aa34803d2271055b9c7e660779e
Juan B Cabral <jbc.develop@gmail.com>
2020-03-25 01:28:17 -0300

    a lot

--- site/ingest/models.py
+++ site/ingest/models.py
@@ -54,3 +54,58 @@ class RawFile(TimeStampedModel):
         parser = self.PARSERS[self.ext[1:]]
         df = parser(self.file.path)
         return df
+
+# =============================================================================
+# REAL MODELS
+# =============================================================================
+
+class Patient(TimeStampedModel):
+
+    patient_raw_file_id = models.IntegerField(
+        unique=True, verbose_name="ID de archivo")
+    gender = models.CharField(max_length=255, verbose_name="género")
+    nationality = models.CharField(max_length=255, verbose_name="nacionalidad")
+    address = models.CharField(max_length=255, verbose_name="dirección")
+    notes = models.TextField(blank=True, verbose_name="notas", null=True)
+
+
+class Sintoms(TimeStampedModel):
+
+        description = models.CharField(
+            max_length=255, verbose_name="description")
+
+
+class InfectionSource(TimeStampedModel):
+
+    desc = models.CharField(max_length=255, verbose_name="Description")
+
+
+class Event(TimeStampedModel):
+
+    STATUSES = {
+        "suspected": "Sospechoso",
+        "active": "Activo",
+        "recovered": "Recuperado",
+        "deceased": "Fallecido"}
+
+    raw_file = models.ForeignKey(
+        RawFile, related_name="events",
+        on_delete=models.CASCADE, verbose_name="Archivo")
+
+    sintoms = models.ManyToManyField(Sintoms, related_name="events")
+
+    event_raw_file_id = models.IntegerField(unique=True)
+    patient = models.ForeignKey(
+        Patient, related_name="events",
+        on_delete=models.CASCADE, verbose_name="Paciente")
+
+    status = models.CharField(
+        max_length=50, choices=list(STATUSES.items()))
+
+    lock_address = models.CharField(max_length=255)
+
+    has_simptoms = models.BooleanField(null=True)
+    first_simptoms_date = models.DateField(null=True)
+    confirmed_date = models.DateField(null=True)
+    deceased_date = models.DateField(null=True)
+    discharge_date = models.DateField(null=True)
