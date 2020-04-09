a1bb41e691554df03e7c20d94241d5e184bfa583
Juan B Cabral <jbc.develop@gmail.com>
2020-04-09 16:22:17 -0300

    command

---
+++ site/brooks/management/commands/resetdemo.py
@@ -0,0 +1,24 @@
+import shutil
+import os
+
+from django.conf import settings
+
+from django.core.management.base import BaseCommand, CommandError
+from django.core.management import call_command
+
+
+class Command(BaseCommand):
+    help = "Clean the database and remove all the uploaded files"
+
+    def handle(self, *args, **options):
+        if not settings.DEMO_MODE:
+            raise CommandError("This command can only run in demo mode")
+
+        print(f"!!! Reset DB")
+        call_command('reset_db', '--noinput')
+        print(f"!!! Migrate")
+        call_command('migrate')
+        print(f"!!! Removing {settings.MEDIA_ROOT}")
+        if os.path.exists(settings.MEDIA_ROOT):
+            shutil.rmtree(settings.MEDIA_ROOT)
+
