c5d7c86a2b5f07c86fa77086e913d256f935150f
Juan B Cabral <jbc.develop@gmail.com>
2020-04-12 13:02:47 -0300

    intermediate

--- site/libs/model_descriptions.py
+++ site/libs/model_descriptions.py
@@ -99,117 +99,124 @@ KEYS_TO_REMOVE = ["format", "tag"]
 # FUNCTIONS
 # =============================================================================
 
-def load_file(filename):
-    """Load the descfile based on the file extension"""
-    ext = os.path.splitext(filename)[-1].lower()
-    parser = PARSERS[ext]
-    with open(filename) as fp:
-        return parser(fp)
+class Compiler:
 
+    def load_file(self, filename):
+        """Load the descfile based on the file extension"""
+        ext = os.path.splitext(filename)[-1].lower()
+        parser = PARSERS[ext]
+        with open(filename) as fp:
+            return parser(fp)
 
-def translate(data):
-    """Traduce todo lo que tiene el diccionario de datos
-    si la llave traducida esta incluida en NO_TRANSLATE.
+    def translate(self, data):
+        """Traduce todo lo que tiene el diccionario de datos
+        si la llave traducida esta incluida en NO_TRANSLATE.
 
-    """
-    if isinstance(data, list):
-        return [translate(e) for e in data]
-    elif isinstance(data, dict):
-        tdict = {}
-        for k, v in data.items():
-            tk = translate(k)
-            tv = v if tk in NO_TRANSLATE else translate(v)
-            tdict[tk] = tv
-        return tdict
-    return DATA_TRANSLATIONS.get(data, data)
+        """
+        if isinstance(data, list):
+            return [self.translate(e) for e in data]
+        elif isinstance(data, dict):
+            tdict = {}
+            for k, v in data.items():
+                tk = self.translate(k)
+                tv = v if tk in NO_TRANSLATE else self.translate(v)
+                tdict[tk] = tv
+            return tdict
+        return DATA_TRANSLATIONS.get(data, data)
 
+    def create_field(self, *, name, data, emodels):
+        # copiamos los datos para manipular tranquilos
+        data = copy.deepcopy(data)
 
-def create_field(*, name, data, emodels):
-    # copiamos los datos para manipular tranquilos
-    data = copy.deepcopy(data)
+        # primero sacamos el tipo
+        ftype = data.pop("type")
+        dj_ftype = FIELD_TYPES.get(ftype, ftype)
 
-    # primero sacamos el tipo
-    ftype = data.pop("type")
-    dj_ftype = FIELD_TYPES.get(ftype, ftype)
+        # aca saco todos las configuraciones que se usaran vara parsear y no
+        # para crear cosas
+        for tr in KEYS_TO_REMOVE:
+            data.pop(tr, None)
 
-    # aca saco todos las configuraciones que se usaran vara parsear y no
-    # para crear cosas
-    for tr in KEYS_TO_REMOVE:
-        data.pop(tr, None)
+        # ahora el tipo puede ser uno nativo o un foreign key
+        if inspect.isclass(dj_ftype) and issubclass(dj_ftype, models.Field):
 
-    # ahora el tipo puede ser uno nativo o un foreign key
-    if inspect.isclass(dj_ftype) and issubclass(dj_ftype, models.Field):
+            if "choices" in data:
+                data["choices"] = [(e, e) for e in data["choices"]]
 
-        if "choices" in data:
-            data["choices"] = [(e, e) for e in data["choices"]]
+            fvalidators = []
+            if "min" in data:
+                min_value = data.pop("min")
+                fvalidators.append(validators.MinValueValidator(min_value))
+            if "max" in data:
+                max_value = data.pop("max")
+                fvalidators.append(validators.MaxValueValidator(max_value))
+            if fvalidators:
+                data["validators"] = fvalidators
 
-        fvalidators = []
-        if "min" in data:
-            min_value = data.pop("min")
-            fvalidators.append(validators.MinValueValidator(min_value))
-        if "max" in data:
-            max_value = data.pop("max")
-            fvalidators.append(validators.MaxValueValidator(max_value))
-        if fvalidators:
-            data["validators"] = fvalidators
+            return dj_ftype(**data)
 
-        return dj_ftype(**data)
-
-    else:
-        link_to = emodels[dj_ftype]
-        # we have a foreign key, si tiene "sep" es un many-to-many
-        sep = data.pop("sep", None)
-        if sep:
-            return models.ManyToManyField(link_to, **data)
         else:
-            return models.ForeignKey(
-                link_to, on_delete=models.CASCADE, **data)
+            # here we have some kind of FK
+            link_to = emodels[dj_ftype]
+
+            # if we have sep is a many to many
+            sep = data.pop("sep", None)
+            if sep:
+                return models.ManyToManyField(link_to, **data)
+            else:
+                return models.ForeignKey(
+                    link_to, on_delete=models.CASCADE, **data)
 
 
-def create_model(*, name, data, emodels, module_spec):
-    # copiamos los datos para manipular tranquilos
-    data = copy.deepcopy(data)
+    def create_model(self, *, name, data, emodels, module_spec):
+        # copiamos los datos para manipular tranquilos
+        data = copy.deepcopy(data)
 
-    # creamos el contenedor de todos los atributos para el modelo
-    attrs = {'__module__': module_spec.name}
+        # creamos el contenedor de todos los atributos para el modelo
+        attrs = {'__module__': module_spec.name}
 
-    # iteramos sobre
-    attributes = data.pop("attributes", {})
-    for field_name, field_data in attributes.items():
-        new_field = create_field(
-            name=field_name, data=field_data, emodels=emodels)
-        attrs[field_name] = new_field
+        # iteramos sobre
+        attributes = data.pop("attributes", {})
+        for field_name, field_data in attributes.items():
+            new_field = self.create_field(
+                name=field_name, data=field_data, emodels=emodels)
+            attrs[field_name] = new_field
 
-    # creamos el modelo en si
-    modelcls = type(name, (TimeStampedModel,), attrs)
+        # creamos el modelo en si
+        modelcls = type(name, (TimeStampedModel,), attrs)
 
-    return modelcls
+        return modelcls
 
+    def mcompile(self, context):
 
-def from_description(*, descfile, context):
+        # determinar y leer el formato del archivo
+        data = self.load_file(self.descfile)
 
-    # determinar y leer el formato del archivo
-    data = load_file(descfile)
+        # sacamos todos los modelos preexistentes en el contexto
+        emodels = {
+            k: v for k, v in context.items()
+            if inspect.isclass(v) and issubclass(v, models.Model)}
+        dmodels = {}
 
-    # sacamos todos los modelos preexistentes en el contexto
-    emodels = {
-        k: v for k, v in context.items()
-        if inspect.isclass(v) and issubclass(v, models.Model)}
+        # buscamos en que modelo nos estan definiendo
+        module_spec = context["__spec__"]
 
-    # buscamos en que modelo nos estan definiendo
-    module_spec = context["__spec__"]
+        # creamos los nuevos modelos en un nuevo diccionario
+        for model_name, model_data in data.items():
 
-    # creamos los nuevos modelos en un nuevo diccionario
-    for model_name, model_data in data.items():
+            # traducimos las llaves del dicionario de datos
+            tmodel_data =  self.translate(model_data)
 
-        # traducimos las llaves del dicionario de datos
-        tmodel_data =  translate(model_data)
+            new_model = self.create_model(
+                name=model_name, data=tmodel_data,
+                emodels=emodels, module_spec=module_spec)
 
-        new_model = create_model(
-            name=model_name, data=tmodel_data,
-            emodels=emodels, module_spec=module_spec)
-        emodels[model_name] = new_model
+            dmodels[model_name] = new_model
+            emodels[model_name] = new_model
 
-    # injectar modelos en el contexto
-    context.update(emodels)
+        # injectar modelos en el contexto
+        context.update(emodels)
+
+        self._compiled = True
+        self._dmodels = dmodels
 
