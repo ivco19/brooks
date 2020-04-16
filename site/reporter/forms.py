3e78ee61395bf164212855cb1ecffe199f230bb0
Juan B Cabral <jbc.develop@gmail.com>
2020-04-16 13:40:53 -0300

    more docs

--- site/reporter/forms.py
+++ site/reporter/forms.py
@@ -1,3 +1,18 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Arcovid-19 Brooks.
+# Copyright (c) 2020, Juan B Cabral, Vanessa Daza, Diego García Lambas,
+#                     Marcelo Lares, Nadia Luczywo, Dante Paz, Rodrigo Quiroga,
+#                     Bruno Sanchez, Federico Stasyszyn.
+# License: BSD-3-Clause
+#   Full Text: https://github.com/ivco19/brooks/blob/master/LICENSE
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
 from django import forms
 
 from crispy_forms.helper import FormHelper
@@ -6,8 +21,12 @@ from crispy_forms.layout import Submit
 from django_summernote.widgets import SummernoteInplaceWidget
 
 
-from . import models
+from reporter import models
 
+
+# =============================================================================
+# CLASSES
+# =============================================================================
 
 class ReportConfigurationForm(forms.ModelForm):
     class Meta:
--- site/reporter/models.py
+++ site/reporter/models.py
@@ -1,7 +1,25 @@
-# models.py
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Arcovid-19 Brooks.
+# Copyright (c) 2020, Juan B Cabral, Vanessa Daza, Diego García Lambas,
+#                     Marcelo Lares, Nadia Luczywo, Dante Paz, Rodrigo Quiroga,
+#                     Bruno Sanchez, Federico Stasyszyn.
+# License: BSD-3-Clause
+#   Full Text: https://github.com/ivco19/brooks/blob/master/LICENSE
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
 from django.db import models
 from solo.models import SingletonModel
 
+
+# =============================================================================
+# MODELS
+# =============================================================================
 
 class ReportConfiguration(SingletonModel):
     class Meta:
