815dabd51e939eb86ef23d095fe3196cbc54357b
Juan B Cabral <jbc.develop@gmail.com>
2020-04-16 13:43:08 -0300

    more docs

--- site/reporter/views.py
+++ site/reporter/views.py
@@ -1,9 +1,28 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# This file is part of Arcovid-19 Brooks.
+# Copyright (c) 2020, Juan B Cabral, Vanessa Daza, Diego García Lambas,
+#                     Marcelo Lares, Nadia Luczywo, Dante Paz, Rodrigo Quiroga,
+#                     Bruno Sanchez, Federico Stasyszyn.
+# License: BSD-3-Clause
+#   Full Text: https://github.com/ivco19/brooks/blob/master/LICENSE
+
+
+# =============================================================================
+# IMPORTS
+# =============================================================================
+
 from django.views.generic import UpdateView
 
 from brooks.views_mixins import LogginRequired
 
 from reporter import models, forms
 
+
+# =============================================================================
+# CLASSES
+# =============================================================================
 
 class ReportConfigurationView(LogginRequired, UpdateView):
     model = models.ReportConfiguration
